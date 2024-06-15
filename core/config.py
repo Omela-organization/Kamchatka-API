d809fd0a55e20e6c44ac31b7395df67b294944a8
Тихонов Иван А. <iatikhonov@f-soft.org>
2024-06-16 01:35:08 +0300

    [NEW] add sentry

--- core/config.py
+++ core/config.py
@@ -8,3 +8,4 @@ class Settings:
     JWT_ACCESS_SECRET_KEY: str = os.getenv("JWT_ACCESS_SECRET_KEY")
     JWT_REFRESH_SECRET_KEY: str = os.getenv("JWT_REFRESH_SECRET_KEY")
     JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM")
+    SENTRY_SDK: str = os.getenv("SENTRY_SDK")
--- main.py
+++ main.py
@@ -3,12 +3,26 @@ import os
 import asyncio
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-
+from core.config import Settings
 from src.db.base_model import init_models, dispose_engine, drop_models
 from src.routers.base_router import base_router
 
 os.makedirs("photos", exist_ok=True)
 os.makedirs("documents", exist_ok=True)
+
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn=Settings.SENTRY_SDK,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
 
 app = FastAPI(
     description="API для ECO_Kamchatka",
